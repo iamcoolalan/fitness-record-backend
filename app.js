2c6f8342a437f4d8156e6778042d1e2c400dba5a
iamcoolalan <iamalan1996@gmail.com>
2023-10-17 11:22:37 +0800

    style: change to commonJS

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-import express from 'express'
-import dotenv from 'dotenv'
-import methodOverride from 'method-override'
-import session from 'express-session'
-
-import routes from './routes/index.js'
-
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: '.env' })
+  require('dotenv').config({ path: '.env' })
 }
+
+const path = require('path')
+
+const express = require('express')
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+const routes = require('./routes/index.js')
 
 const app = express()
 const port = process.env.PORT || 3000
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
