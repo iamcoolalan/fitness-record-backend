103e75b4ca046b3c8f9208b9452cc3825b715908
iamcoolalan <iamalan1996@gmail.com>
2023-10-27 15:57:24 +0800

    feat: add 404 not found middleware

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const loginSystemController = require('../controllers/login-system-controller')
 const { loginAuth, permissionAuth } = require('../middlewares/auth')
 const { loginValidation, targetValidation, infoValidation } = require('../middlewares/validation')
 const { errorHandler } = require('../middlewares/error-handler')
+const { CustomError } = require('../helpers/error-handler-helpers')
 
 const bodydataRecord = require('./modules/bodydata-record')
 const workoutRecord = require('./modules/workout-record')
@@ -19,8 +20,14 @@ router.use('/user', permissionAuth, user)
 router.post('/login', loginValidation, loginAuth, loginSystemController.login)
 router.post('/signup', infoValidation, targetValidation, loginSystemController.signup)
 
-router.get('*', permissionAuth, (req, res) => {
-  res.send('hi')
+router.use((req, res, next) => {
+  const newError = new CustomError('Sorry, we cannot find that!', {
+    type: 'Not Found Error',
+    from: 'Server',
+    detail: '404 Not Found'
+  })
+
+  next(newError)
 })
 
 router.use('/', errorHandler)
